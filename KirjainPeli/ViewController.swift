e7dfdcfe9d06ff3212337daa32544fb289b35769
Timo Olkkonen <timo.olkkonen@reaktor.com>
2016-04-09 12:36:58 +0300

    Add max characters.

--- KirjainPeli/ViewController.swift
+++ KirjainPeli/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
     
     let SPACING: CGFloat = 20
     let FONT_SIZE: CGFloat = 40
+    let MAX_CHARS = 30
     
     let label: UILabel = UILabel.init()
     let scoreLabel: UILabel = UILabel.init()
@@ -24,7 +25,8 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
     
     var db: COpaquePointer = nil
     var statement: COpaquePointer = nil
-    var utteranceIndexes = [AVSpeechUtterance: Int]()
+    var pendingUtterances = [AVSpeechUtterance: Int]()
+    var startedUtterances = [AVSpeechUtterance: Int]()
     var utteranceScores = [AVSpeechUtterance: (Int, Int)]()
     
     override func viewDidLoad() {
@@ -104,20 +106,31 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
     
     func letterPressed(letterButton: UIButton) {
         let letter = letterButton.currentTitle!
-        characterBuffer.appendAttributedString(NSAttributedString.init(string: letter))
         let characterCount = characterBuffer.string.characters.count
+        if (characterCount == MAX_CHARS) {
+            characterBuffer = NSMutableAttributedString.init(string: characterBuffer.string.substringFromIndex(characterBuffer.string.startIndex.successor()))
+            for (utterance, index) in pendingUtterances {
+                pendingUtterances[utterance] = index - 1
+            }
+            for (utterance, index) in startedUtterances {
+                let newIndex = index - 1
+                startedUtterances[utterance] = newIndex
+                highlight(utterance, startIndex: newIndex)
+            }
+        }
+        characterBuffer.appendAttributedString(NSAttributedString.init(string: letter))
         label.attributedText = characterBuffer
         
         let letterUtterance = toUtterance(letter)
         speechSynthesizer.speakUtterance(letterUtterance)
-        utteranceIndexes[letterUtterance] = characterCount - letterUtterance.speechString.characters.count
+        pendingUtterances[letterUtterance] = characterCount - letterUtterance.speechString.characters.count
         
         let words = getWords()
         let utterances = words.map(toUtterance)
         utterances.forEach({
             let utteranceLength = $0.speechString.characters.count
             utteranceScores[$0] = (utteranceLength, utterances.count)
-            utteranceIndexes[$0] = characterCount - utteranceLength
+            pendingUtterances[$0] = characterCount - utteranceLength
             speechSynthesizer.speakUtterance($0)
         })
     }
@@ -169,26 +182,55 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
     }
 
     func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didStartSpeechUtterance utterance: AVSpeechUtterance) {
-        let startIndex = utteranceIndexes[utterance]
-        characterBuffer.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSRange(location: startIndex!, length: utterance.speechString.characters.count))
-        label.attributedText = characterBuffer
-        
-        if let scores = utteranceScores[utterance] {
-            incrementLabel.text = "+ \(scores.1)x\(scores.0)"
+        dispatch_async(dispatch_get_main_queue()) { [weak self] in
+            if let store = self {
+                let startIndex = store.pendingUtterances[utterance]
+                store.highlight(utterance, startIndex: startIndex)
+                store.label.attributedText = store.characterBuffer
+                if let scores = store.utteranceScores[utterance] {
+                    store.incrementLabel.text = "+ \(scores.1)x\(scores.0)"
+                }
+                store.pendingUtterances.removeValueForKey(utterance)
+                store.startedUtterances[utterance] = startIndex
+            }
         }
     }
     
     func speechSynthesizer(synthesizer: AVSpeechSynthesizer, didFinishSpeechUtterance utterance: AVSpeechUtterance) {
-        let startIndex = utteranceIndexes[utterance]
-        characterBuffer.removeAttribute(NSForegroundColorAttributeName, range: NSRange(location: startIndex!, length: utterance.speechString.characters.count))
-        label.attributedText = characterBuffer
-        if let utteranceScore = utteranceScores[utterance] {
-            score += utteranceScore.0 * utteranceScore.1
-            scoreLabel.text = "\(score)"
-            incrementLabel.text = " "
+        dispatch_async(dispatch_get_main_queue()) { [weak self] in
+            if let store = self {
+                let startIndex = store.startedUtterances[utterance]
+                if let startLength = store.adjustedStartAndLength(startIndex, utterance: utterance) {
+                    store.characterBuffer.removeAttribute(NSForegroundColorAttributeName, range: NSRange(location: startLength.start, length: startLength.length))
+                }
+                store.label.attributedText = store.characterBuffer
+                if let utteranceScore = store.utteranceScores[utterance] {
+                    store.score += utteranceScore.0 * utteranceScore.1
+                    store.scoreLabel.text = "\(store.score)"
+                    store.incrementLabel.text = " "
+                }
+                store.utteranceScores.removeValueForKey(utterance)
+                store.startedUtterances.removeValueForKey(utterance)
+            }
         }
-        utteranceScores.removeValueForKey(utterance)
-        utteranceIndexes.removeValueForKey(utterance)
+    }
+    
+    func highlight(utterance: AVSpeechUtterance, startIndex: Int?) {
+        if let startLength = adjustedStartAndLength(startIndex, utterance: utterance) {
+            characterBuffer.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSRange(location: startLength.start, length: startLength.length))
+            }
+    }
+    
+    func adjustedStartAndLength(startIndex: Int?, utterance: AVSpeechUtterance) -> (start: Int, length: Int)? {
+        if let index = startIndex {
+            let length = utterance.speechString.characters.count
+            let adjustedIndex = index < 0 ? 0 : index
+            let adjustedLength = index < 0 ? index + length : length
+            if (adjustedLength > 0) {
+                return (adjustedIndex, adjustedLength)
+            }
+        }
+        return nil
     }
     
     override func didReceiveMemoryWarning() {
